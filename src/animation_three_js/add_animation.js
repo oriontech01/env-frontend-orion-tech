e60d6da136cf5cea22e9f6ddf2de61331ac1a969
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2024-01-24 12:53:30 +0100

    Update add_animation.js

--- src/animation_three_js/add_animation.js
+++ src/animation_three_js/add_animation.js
@@ -5,6 +5,7 @@ import background from './assets/images/background2.jpg';
 import { ModelAnimation } from "./animation_components/model_animation";
 import { AmbientLight } from "./animation_components/ambient_light";
 import { Point_Light } from "./animation_components/point_light";
+import { Loading } from "./loading";
 
 
 
@@ -14,7 +15,7 @@ export const AddAnimation= (props) => {
   return (
   <div style={{backgroundImage: `url(${background})`}} className="h-screen">
     <Canvas camera={{fov:70, position: [0, 15, 55]}}>
-      <Suspense fallback={null}>
+      <Suspense fallback={<Loading />}>
         <AmbientLight />
         <Point_Light />
         <ModelAnimation props= {props}/>
